68d5f97042534048693a8cddfa16a90a4f3223d0
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-11-19 16:17:37 +0100

    added email/pw login

--- src/Containers/LoginScreen.js
+++ src/Containers/LoginScreen.js
@@ -1,14 +1,21 @@
 import React from "react";
 import * as Realm from "realm-web";
+import styled from "@emotion/styled";
+import TextInput from "@leafygreen-ui/text-input";
 import Button from "@leafygreen-ui/button";
+import validator from "validator";
+
 import { useRealmApp } from "../RealmApp";
 import Container from "../Components/Container";
 
 const LoginScreen = () => {
     const app = useRealmApp();
+    const [email, setEmail] = React.useState("philip@eschenbacher.ch");
+    const [password, setPassword] = React.useState("Passw0rd");
+
     const handleLogin = async () => {
         try {
-            await app.logIn(Realm.Credentials.anonymous());
+            await app.logIn(Realm.Credentials.emailPassword(email, password));
         } catch (e) {
             console.error("error loggin in");
         }
@@ -16,7 +23,25 @@ const LoginScreen = () => {
 
     return (
         <Container>
-            <h1>Please identify</h1>
+            <LoginFormRow>
+                <LoginHeading>Please identify</LoginHeading>
+            </LoginFormRow>
+            <LoginFormRow>
+                <TextInput 
+                    type="email" 
+                    label="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </LoginFormRow>
+            <LoginFormRow>
+                <TextInput 
+                    type="password" 
+                    label="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </LoginFormRow>            
             <Button variant="primary" onClick={() => handleLogin()}>Login</Button>
         </Container>
     )
@@ -24,3 +49,11 @@ const LoginScreen = () => {
 
 export default LoginScreen;
 
+const LoginHeading = styled.h1`
+  margin: 0;
+  font-size: 32px;
+`;
+
+const LoginFormRow = styled.div`
+  margin-bottom: 16px;
+`;
