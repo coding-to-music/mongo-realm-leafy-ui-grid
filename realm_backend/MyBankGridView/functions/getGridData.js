13b14f430d05edfb717fdcbe18242ccc29b9a054
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-11-26 15:19:30 +0100

    added backend

---
+++ realm_backend/MyBankGridView/functions/getGridData.js
@@ -0,0 +1,19 @@
+exports = async ({ startRow, endRow }) => {
+  const cluster = context.services.get("mongodb-atlas");
+  const collection = cluster.db("mybank").collection("customerSingleviewFlat");
+  
+  return await collection.aggregate([{"$skip": startRow}, {"$limit": endRow-startRow}]).toArray();
+}
+
+/** 
+ * TESTDATA
+ * copy this to console section
+ 
+exports(
+  {
+    startRow: 0, 
+    endRow: 5
+  }  
+)
+*/
+ 
